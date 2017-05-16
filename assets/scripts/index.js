0b836b2e67a1b04a4b1b7a88ccbd97b5fcd56081
artpylon <matthewgray88@gmail.com>
2017-05-16 11:09:11 -0400

    Added event to index.js for signup

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,14 +2,15 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+require('./events')
 
 $(() => {
   setAPIOrigin(location, config)
-
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
 })
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
-require('./example')
